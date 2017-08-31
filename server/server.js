e38f1ec7661663259d64bf3ddd6381470dd69ba2
Alexander <aiadamov182@gmail.com>
2017-08-31 09:59:31 +0300

    Add socket.io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+const server = http.createServer(app);
+// Create websockets server.
+const io = socketIO(server);
+
+// Register an event listener.
+// Listen for a new connection from client.
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    // socket - socket instance from client.
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    });
+});
+
 app.use(express.static(publicPath));
-app.listen(port, () => {
+
+server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
