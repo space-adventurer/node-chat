2214e09aeac0fd1da70d7dda9ef7dc6bab078c0a
Alexander <aiadamov182@gmail.com>
2017-08-31 08:59:38 +0300

    Handle heroku port

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.static(publicPath));
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
